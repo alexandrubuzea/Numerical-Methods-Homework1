03b74995a8f7ef14316b65bbf10e8b5ffbdcc26d
alexandrubuzea <alexandru.buzea2007@stud.acs.upb.ro>
2023-04-24 15:21:45 +0300

    update/added-checker

---
+++ markov-is-coming/get_adjacency_matrix.m
@@ -0,0 +1,8 @@
+function [Adj] = get_adjacency_matrix (Labyrinth)
+	% Labyrinth -> the matrix of all encodings
+  
+  % Adj -> the adjacency matrix associated to the given labyrinth
+  
+  % TODO: get_adjacency_matrix implementation
+  
+endfunction
---
+++ markov-is-coming/get_link_matrix.m
@@ -0,0 +1,8 @@
+function [Link] = get_link_matrix (Labyrinth)
+	% Labyrinth -> the matrix of all encodings
+  
+  % Link -> the link matrix associated to the given labyrinth
+  
+  % TODO: get_link_matrix implementation
+  
+endfunction
